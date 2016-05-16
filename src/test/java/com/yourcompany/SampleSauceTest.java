a077fd61f396889e75c81224c8832010fe167fcb
Leo Laskin <llaskin@gmail.com>
2016-05-16 13:03:01 -0500

    initial commit

---
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -0,0 +1,164 @@
+package com.saucelabs;
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.junit.ConcurrentParameterized;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.LinkedList;
+
+/**
+ * Demonstrates how to write a JUnit test that runs tests against Sauce Labs using multiple browsers in parallel.
+ * <p/>
+ * The test also includes the {@link SauceOnDemandTestWatcher} which will invoke the Sauce REST API to mark
+ * the test as passed or failed.
+ *
+ * @author Ross Rowe
+ */
+@RunWith(ConcurrentParameterized.class)
+public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
+
+    /**
+     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
+     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
+     */
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication();
+
+    /**
+     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
+     */
+    @Rule
+    public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    /**
+     * Represents the browser to be used as part of the test run.
+     */
+    private String browser;
+    /**
+     * Represents the operating system to be used as part of the test run.
+     */
+    private String os;
+    /**
+     * Represents the version of the browser to be used as part of the test run.
+     */
+    private String version;
+    /**
+     * Instance variable which contains the Sauce Job Id.
+     */
+    private String sessionId;
+
+    /**
+     * The {@link WebDriver} instance which is used to perform browser interactions with.
+     */
+    private WebDriver driver;
+
+    /**
+     * Constructs a new instance of the test.  The constructor requires three string parameters, which represent the operating
+     * system, version and browser to be used when launching a Sauce VM.  The order of the parameters should be the same
+     * as that of the elements within the {@link #browsersStrings()} method.
+     * @param os
+     * @param version
+     * @param browser
+     */
+    public SampleSauceTest(String os, String version, String browser) {
+        super();
+        this.os = os;
+        this.version = version;
+        this.browser = browser;
+    }
+
+    /**
+     * @return a LinkedList containing String arrays representing the browser combinations the test should be run against. The values
+     * in the String array are used as part of the invocation of the test constructor
+     */
+    @ConcurrentParameterized.Parameters
+    public static LinkedList browsersStrings() {
+        LinkedList browsers = new LinkedList();
+        browsers.add(new String[]{"Windows 7", "45", "Chrome"});
+        browsers.add(new String[]{"OS X 10.8", "6", "safari"});
+        browsers.add(new String[]{"Windows 7", "45", "Chrome"});
+        browsers.add(new String[]{"Windows 8", "10", "internet explorer"});
+        browsers.add(new String[]{"OS X 10.10", "8.0", "safari"});
+        browsers.add(new String[]{"Windows 7", "5.1", "safari"});
+        browsers.add(new String[]{"Windows XP", "7.0", "internet explorer"});
+        browsers.add(new String[]{"OS X 10.9", "7.0", "safari"});
+        browsers.add(new String[]{"Windows 8.1", "38.0", "firefox"});
+        browsers.add(new String[]{"Windows 8.1", "43.0", "Chrome"});
+
+        return browsers;
+    }
+
+
+    /**
+     * Constructs a new {@link RemoteWebDriver} instance which is configured to use the capabilities defined by the {@link #browser},
+     * {@link #version} and {@link #os} instance variables, and which is configured to run against ondemand.saucelabs.com, using
+     * the username and access key populated by the {@link #authentication} instance.
+     *
+     * @throws Exception if an error occurs during the creation of the {@link RemoteWebDriver} instance.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        String username = "leo-10-tester";
+        String key = "59db7689-b610-42c7-bc02-e5aef89c2307";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        if (version != null) {
+            capabilities.setCapability(CapabilityType.VERSION, version);
+        }
+        capabilities.setCapability(CapabilityType.PLATFORM, os);
+        capabilities.setCapability("name", "The Internet Parallel Java");
+		this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+                this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+
+    }
+
+    /**
+     * Runs a simple test validating against The Internet.
+     * @throws Exception
+     */
+    @Test
+    public void sauce() throws Exception {
+        driver.get("http://the-internet.herokuapp.com/secure");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button.radius")).click();
+        System.out.println("Page header is: " + driver.getTitle());
+         if (!driver.findElement(By.tagName("html")).getText().contains("Secure Area")) {
+            System.out.println("verifyTextPresent failed");
+        }
+    }
+
+
+    /**
+     * Closes the {@link WebDriver} session.
+     *
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    /**
+     *
+     * @return the value of the Sauce Job id.
+     */
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+}
